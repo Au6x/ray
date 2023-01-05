312e3f5e86ae8d62d2484168c2f5a79f25fc8d23
Raymond Sekgobela <115866086+Au6x@users.noreply.github.com>
2023-01-05 15:35:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.write("---")
 #with st.sidebar:
 selected = option_menu(
   menu_title=None,
-            options=["  Sales  ","Segments","Marketing"],
+            options=["Sales Analysis","Segments","Marketing"],
             icons=["bar-chart-line","pie-chart","diagram-3"],
             menu_icon="person",
             default_index=0,
@@ -29,7 +29,7 @@ selected = option_menu(
 # Add a title to the app
 
 
-if selected == "  Sales  ":
+if selected == "Sales Analysis":
     st.write("---")
     st.header("Sales Analysis")
     st.write("---")
